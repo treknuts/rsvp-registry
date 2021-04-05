fa6744fc61faa8af903d55cda441fcc8b6cbbc9e
treknuts <trevor.a.knutson@ndsu.edu>
2021-04-05 13:29:27 -0500

    Added form functionality.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Main from "./components/Main";
-import Navbar from "./components/Navbar";
-import { faBook, faCheck, faGifts } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBook,
+  faCheck,
+  faGifts,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function App() {
   const [isRSVP, setIsRSVP] = useState(true);
@@ -23,32 +27,53 @@ function App() {
       setIsRegistry(false);
       setIsAbout(true);
     }
-    e.preventDefault()
-  } 
+    e.preventDefault();
+  };
 
   return (
     <div className="lg:flex flex-col h-screen w-screen font-sans">
-      <div className="absolute flex flex-row justify-center text-blue font-bold bottom-0 h-12 w-screen lg:my-8 shadow-2xl lg:flex-col lg:inset-y-0 lg:left-0 lg:h-full lg:w-64 lg:shadow-2xl lg:rounded-2xl bg-pink-200 lg:transition lg:ease-in-out lg:transform lg:hover:translate-x-3">
-      <div id="rsvp" onClick={handleClick} className="relative hover:animate-bounce hover:cursor-pointer lg:hover:animate-none flex items-center justify-center rounded-2xl lg:w-full w-8 lg:my-24 lg:pl-8 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200">
-        <p className="relative hidden lg:contents lg:mx-auto" href="#">
-          RSVP
-        </p>
-        <FontAwesomeIcon className="color-blue lg:mx-auto" icon={faCheck} />
+      <div className="absolute flex flex-row justify-center text-blue font-bold bottom-0 h-12 lg:py-8 w-screen lg:my-8 shadow-2xl lg:flex-col lg:inset-y-0 lg:left-0 lg:h-full lg:w-64 lg:shadow-2xl lg:rounded-2xl bg-pink-200 lg:transition lg:ease-in-out lg:transform lg:hover:translate-x-3">
+        <div
+          id="rsvp"
+          onClick={handleClick}
+          className="relative hover:animate-bounce hover:cursor-pointer lg:hover:animate-none flex items-center justify-center rounded-2xl lg:w-full w-8 lg:my-24 lg:pl-8 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200"
+        >
+          <p className="relative hidden lg:contents lg:mx-auto" href="#">
+            RSVP
+          </p>
+          <FontAwesomeIcon className="color-blue lg:mx-auto" icon={faCheck} />
+        </div>
+        <div className="relative hover:animate-bounce lg:hover:animate-none flex items-center justify-center rounded-2xl lg:w-full w-8 lg:my-24 lg:pl-8 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200">
+          <a className="hidden lg:contents lg:mx-auto" href="#">
+            Info
+          </a>
+          <FontAwesomeIcon className="my-auto lg:mx-auto" icon={faInfoCircle} />
+        </div>
+        <div
+          id="registry"
+          onClick={handleClick}
+          className="relative hover:animate-bounce hover:cursor-pointer lg:hover:animate-none flex items-center justify-center rounded-2xl lg:w-full w-8 lg:my-24 lg:pl-8 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200"
+        >
+          <p className="hidden lg:contents lg:mx-auto" href="#">
+            Registry
+          </p>
+          <FontAwesomeIcon className="my-auto lg:mx-auto" icon={faGifts} />
+        </div>
+        <div
+          id="about"
+          onClick={handleClick}
+          className="relative hover:animate-bounce lg:hover:animate-none hover:cursor-pointer flex items-center justify-center lg:justify-evenly rounded-2xl lg:w-full lg:pl-8 w-8 lg:my-24 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200"
+        >
+          <p className="hidden lg:contents sm:mx-auto" href="#">
+            Our Story
+          </p>
+          <FontAwesomeIcon
+            className="color-blue my-auto lg:mx-auto"
+            icon={faBook}
+          />
+        </div>
       </div>
-      <div id="registry" onClick={handleClick} className="relative hover:animate-bounce hover:cursor-pointer lg:hover:animate-none flex items-center justify-center rounded-2xl lg:w-full w-8 lg:my-24 lg:pl-8 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200">
-        <p className="hidden lg:contents lg:mx-auto" href="#">
-          Registry
-        </p>
-        <FontAwesomeIcon className="my-auto lg:mx-auto" icon={faGifts} />
-      </div>
-      <div id="about" onClick={handleClick} className="relative hover:animate-bounce lg:hover:animate-none hover:cursor-pointer flex items-center justify-center lg:justify-evenly rounded-2xl lg:w-full lg:pl-8 w-8 lg:my-24 lg:h-12 mx-auto hover:bg-blue hover:text-pink-200">
-        <p className="hidden lg:contents sm:mx-auto" href="#">
-          Our Story
-        </p>
-        <FontAwesomeIcon className="color-blue my-auto lg:mx-auto" icon={faBook} />
-      </div>
-    </div>
-      <Main isRSVP={isRSVP} isRegistry={isRegistry} isAbout={isAbout}/>
+      <Main isRSVP={isRSVP} isRegistry={isRegistry} isAbout={isAbout} />
     </div>
   );
 }
--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,74 +1,127 @@
 import React, { useState } from "react";
 
 const RSVP = () => {
-  const [familyName, setFamilyName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [members, setMembers] = useState(1);
 
   const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setFamilyName(e.target.value);
+    if (e.target.id === "first-name") {
+      setFirstName(e.target.value);
+    } else if (e.target.id === "last-name") {
+      setLastName(e.target.value);
+    } else if (e.target.id === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.id === "members") {
+      setMembers(e.target.value);
     }
   };
 
   const handleForm = () => {
-    alert(`Form values are:\nName: ${familyName}`);
+    alert(
+      `Form values are:\n\tFirst Name: ${firstName}\n\tLast Name: ${lastName}\n\tEmail: ${email}\n\Members: ${members}`
+    );
   };
 
   return (
     <div>
-      <form className="w-full max-w-sm">
+      <form className="w-full max-w-lg">
         <h1 className="text-3xl font-bold mb-12">Let us know you're coming!</h1>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-full-name"
-            >
-              Full Name
-            </label>
+        <div className="md:grid md:grid-cols-2 gap-4">
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+                for="first-name"
+              >
+                First
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue"
+                id="first-name"
+                type="text"
+                placeholder="Bob"
+                value={firstName}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
-              type="text"
-              value="Jane Doe"
-            />
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                for="last-name"
+              >
+                Last
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue"
+                id="last-name"
+                type="text"
+                placeholder="Ross"
+                value={lastName}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-password"
-            >
-              Password
-            </label>
+          <div className="md:flex md:items-center mb-6 col-span-2">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+                for="email"
+              >
+                Email
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue"
+                id="email"
+                type="email"
+                placeholder="youremail@domain.com"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-password"
-              type="password"
-              placeholder="******************"
-            />
+          <div className="md:flex md:items-center mb-6 col-span-2">
+            <div className="md:w-2/3">
+              <label
+                className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4"
+                for="members"
+              >
+                Number Attending
+              </label>
+            </div>
+            <div className="md:w-1/3 md:ml-auto">
+              <select
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue text-center"
+                id="members"
+                value={members}
+                onChange={handleChange}
+              >
+                {[1, 2, 3, 4, 5].map((num) => {
+                  return <option value={num}>{num}</option>;
+                })}
+              </select>
+            </div>
           </div>
-        </div>
-        <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3"></div>
-          <label className="md:w-2/3 block text-gray-500 font-bold">
-            <input className="mr-2 leading-tight" type="checkbox" />
-            <span className="text-sm">Send me your newsletter!</span>
-          </label>
-        </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button
-              className="shadow bg-blue hover:bg-green focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="button"
-            >
-              Sign Up
-            </button>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-blue hover:bg-green focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+                onClick={handleForm}
+              >
+                Register!
+              </button>
+            </div>
           </div>
         </div>
       </form>
