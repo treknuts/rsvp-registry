96fd4be74a66b5db4530dfc3bb6a6022813aa62d
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2022-07-05 13:47:35 -0500

    Removed unused import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,3 @@
-import Announcements from "./Announcements";
-
 const Home = () => {
   return (
     <section className='main-content d-flex justify-content-center align-items-center p-2 p-md-4 rounded shadow bg-light'>
