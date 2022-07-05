92eddfd637fb08686f7ec99538c731b8be4e3595
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2022-07-05 13:47:10 -0500

    Removed everything then added thank you. Should decomission soon.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import Announcements from "./Announcements";
 const Home = () => {
   return (
     <section className='main-content d-flex justify-content-center align-items-center p-2 p-md-4 rounded shadow bg-light'>
-      <Announcements />
+      <div>
+        <h3 style={{ fontWeight: "lighter" }} className='text-center'>
+          Thanks to everyone that came to celebrate our big day with us.
+        </h3>
+        <br />
+        <h3 style={{ fontWeight: "lighter" }} className='text-center'>
+          A special thanks to all the friends and family that did so much
+          working leading up to the wedding. We could not have done it without
+          you!
+        </h3>
+      </div>
     </section>
   );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,9 @@
 import Home from "./Home";
-import Registry from "./Registry";
-import Info from "./Info";
 
 const Main = () => {
   return (
-    <main
-      style={{ marginTop: "6rem" }}
-      className='min-vh-100 p-md-0 p-4 mb-4 d-flex flex-column justify-content-center align-items-center'
-    >
+    <main className='min-vh-100 p-md-0 p-4 mb-4 d-flex flex-column justify-content-center align-items-center'>
       <Home />
-      <Info />
-      <Registry />
     </main>
   );
 };
